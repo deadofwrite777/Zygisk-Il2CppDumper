03cbc3a345e1bcb51f15f00a7668a5e56d3fa779
deadofwrite777 <deadofwrite777@gmail.com>
2026-09-24 05:21:55 -0600

    Update hack.cpp

--- module/src/main/cpp/hack.cpp
+++ module/src/main/cpp/hack.cpp
@@ -301,7 +301,7 @@ void injected_entry() {
     LOGI("Running in process: '%s'", cmdline);
     
     const char *game_data_dir = "/data/data/com.mobile.legends";
-    std::thread(hack_start, game_data_dir).detach();
+    game_data_dir.detach();
 }
 
 #endif
